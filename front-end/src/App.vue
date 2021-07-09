ab73022f852f4cc6786f61d6daa1ab01345c2955
danielbrewer <danielbrewer4@gmail.com>
2021-07-09 08:36:20 -0600

    Project Structure set up, header added.

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,28 +1,255 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="../src/assets/Color_logo_no_background.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="title">
+      <h1>Modini</h1>
+    </div>
+    <header id="header">
+      <div id="logo">
+        <router-link to="/"><img alt="Modini logo" src="../src/assets/symbol.png"></router-link>
+      </div>
+      <div class = header-item>
+        <router-link to="/">
+          <p>Home</p>
+        </router-link>
+      </div>
+      <div class = header-item>
+        <router-link to="/about-us">
+          <p>About Us</p>
+        </router-link>
+      </div>
+      <div class = header-item>
+        <router-link to="/services">
+          <p>Services</p>
+        </router-link>
+      </div>
+      <div class = header-item>
+        <router-link to="/contact-us">
+          <p>Contact Us</p>
+        </router-link>
+      </div>
+      <div v-if="user" class = header-item>
+        <router-link to="/user">
+          <p>Account</p>
+        </router-link>
+      </div>
+      <div class = "header-item login">
+        <router-link to="/user">
+          <p v-if="user" @click="logout">Logout<br>{{user.username}}</p>
+          <p v-else>Sign Up / Login</p>
+        </router-link>
+      </div>
+
+
+      <div class="dropdown">
+        <button @click="myFunction()" class="dropbtn">MENU</button>
+        <div id="myDropdown" class="dropdown-content">
+          <router-link to="/"><p>Home</p></router-link>
+          <router-link to="/about-us"><p>About Us</p></router-link>
+          <router-link to="/services"><p>Services</p></router-link>
+          <router-link to="/contact-us"><p>Contact Us</p></router-link>
+          <router-link v-if="user" to="/user">
+            <p>Account</p>
+          </router-link>
+          <router-link to="/user">
+            <p v-if="user" @click="logout">Logout<br>{{user.username}}</p>
+            <p v-else>Sign Up / Login</p>
+          </router-link>
+        </div>
+      </div>
+
+    </header>
+    <router-view />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import axios from 'axios';
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  computed: {
+    user() {
+      return this.$root.$data.user;
+    }
+  },
+  methods: {
+    myFunction() {
+      document.getElementById("myDropdown").classList.toggle("show");
+    },
+    async logout() {
+      try {
+        await axios.delete("/api/users");
+        this.$root.$data.user = null;
+      } catch (error) {
+        this.$root.$data.user = null;
+      }
+    },
+  },
+}
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
   }
 }
 </script>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  align-items: center;
+}
+body {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Work Sans', sans-serif;
+  font-weight: 300;
+  font-size: 13pt;
+  background-color: #fff;
+
+}
+#header{
+  display: flex;
+  position: fixed;
+  background-color: #e2f2f9;
+  width: 100%;
+  height: 120px;
+  font-size: 24px;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+}
+#header img {
+  height: 100px;
+  margin-top: 10px;
+}
+.header-item {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+
+}
+#logo {
+  justify-content: left;
+  transition: transform .7s ease-in-out;
+}
+
+#logo:hover {
+  transform: rotate(180deg);
+}
+
+.header-item a { /* Unvisited link */
+  text-decoration: none;
+}
+.header-item a:visited { /* Visited link */
+  text-decoration: none;
+  color: inherit;
+}
+.header-item a:hover { /* Mouse over link */
+  text-decoration: none;
+  color: #8ecce7;
+}
+.header-item a:active { /* Selected Link */
+  text-decoration: none;
+}
+#header .dropdown {
+  display: none;
+}
+.title {
+  width: 100%;
+  color: #1a2c5b;
+  padding-top: 130px;
+  font-weight: bolder;
+  font-size: 100px;
+  background: #8ecce7;
+}
+
+
+@media (max-width: 960px) {
+  .header-item {
+    display: none;
+  }
+  #header .dropdown {
+    display: inline-block;
+  }
+  #header{
+    align-items: center;
+  }
+  /* Dropdown Button */
+  .dropbtn {
+    background-color: #8ecce7;
+    border-radius: 10px;
+    color: black;
+    padding: 16px;
+    font-size: 16px;
+    border: none;
+    cursor: pointer;
+    box-shadow: 1px 2px #253f83;
+  }
+
+  /* Dropdown button on hover & focus */
+  .dropbtn:hover, .dropbtn:focus {
+    background-color: #1a2c5b;
+    color: white;
+  }
+
+  /* The container <div> - needed to position the dropdown content */
+  .dropdown {
+    position: relative;
+    display: inline-block;
+  }
+
+  /* Dropdown Content (Hidden by Default) */
+  .dropdown-content {
+    display: none;
+    position: absolute;
+    background-color: #f1f1f1;
+    min-width: 160px;
+    box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+    z-index: 1;
+    right: 0;
+  }
+
+  /* Links inside the dropdown */
+  .dropdown-content a {
+    color: black;
+    padding: 12px 16px;
+    text-decoration: none;
+    display: block;
+    border-bottom: solid #212121 1px;
+  }
+  .dropdown-content a:last-child {
+    border-bottom: none;
+  }
+
+  /* Change color of dropdown links on hover */
+  .dropdown-content a:hover {background-color: #ddd}
+
+  /* Show the dropdown menu (use JS to add this class to the .dropdown-content container when the user clicks on the dropdown button) */
+  .show {display:block;}
+
+  .title {
+    font-size: 50px;
+  }
+
 }
 </style>
---
+++ front-end/src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="body">
+    <p>Thanks for visiting our website! It will be up and running Soon!</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home"
+}
+</script>
+
+<style scoped>
+.body {
+  text-align: center;
+  padding-top: 30px;
+  font-size: 50px;
+  font-weight: bolder;
+}
+</style>
